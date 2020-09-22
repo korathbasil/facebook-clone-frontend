3423cda8314161be99bada38482388d97f054450
korathbasil <korathbasil@live.com>
2020-09-22 19:40:32 +0530

    finished login form

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,10 +1,30 @@
 import React from "react";
 import "./Login.css";
+import FBLogo from "./facebook-logo.png";
 
 function Login() {
   return (
     <div className="login">
-      <h2>Bazil Korath</h2>
+      <div className="login__contents">
+        <div className="login__left">
+          <img src={FBLogo} alt="" className="login__leftLogo" />
+          <h2>
+            <span>Facebook helps you to connect and share</span>
+            <span>with the people in your life.</span>
+          </h2>
+        </div>
+        <div className="login__right">
+          <form action="" className="login__rightForm">
+            <input type="email" placeholder="Email address or phone number" />
+            <input type="passsword" placeholder="Password" />
+            <button type="submit">Log in</button>
+          </form>
+          <p>Forgot password?</p>
+          <div className="login__rightBottom">
+            <button>Create New Account</button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
