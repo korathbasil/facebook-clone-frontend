b23a0e7ac259006ef32293030fb43fc535ba2a3a
korathbasil <korathbasil@live.com>
2020-10-04 23:33:29 +0530

    renamed classes

--- src/components/MainBody/Profile/Timeline/Timeline.jsx
+++ src/components/MainBody/Profile/Timeline/Timeline.jsx
@@ -6,58 +6,56 @@ import Post from "../../Home/Feed/Post/Post";
 function Timeline({ photos }) {
   return (
     <div className="timeline">
-      <div className="timeline__">
-        <div className="timeline__container">
-          <div className="timeline__Requst">
-            <div className="timeline__RequstLeft">
-              <h3>Do you know David Beckham</h3>
-            </div>
-            <div className="timeline__RequstRight"></div>
+      <div className="timeline__container">
+        <div className="timeline__request">
+          <div className="timeline__requestLeft">
+            <h3>Do you know David Beckham</h3>
           </div>
-          <div className="timeline__Bottom">
-            <div className="timeline__BottomLeft">
-              <div className="timeline__BottomPhotos">
-                <div className="timeline__BottomPhotosTop">
-                  <h3>Photos</h3>
-                  <p>See all</p>
-                </div>
-                <div className="timeline__BottomPhotosContainer">
-                  {photos.map((photo) => {
-                    return <img src={photo.src} alt="" />;
-                  })}
+          <div className="timeline__requestRight"></div>
+        </div>
+        <div className="timeline__bottom">
+          <div className="timeline__bottomLeft">
+            <div className="timeline__bottomPhotos">
+              <div className="timeline__bottomPhotosTop">
+                <h3>Photos</h3>
+                <p>See all</p>
+              </div>
+              <div className="timeline__bottomPhotosContainer">
+                {photos.map((photo) => {
+                  return <img src={photo.src} alt="" />;
+                })}
 
-                  <img
-                    src="https://www.pandasecurity.com/mediacenter/src/uploads/2013/11/pandasecurity-facebook-photo-privacy.jpg"
-                    alt=""
-                  />
-                  <img
-                    src="https://www.pandasecurity.com/mediacenter/src/uploads/2013/11/pandasecurity-facebook-photo-privacy.jpg"
-                    alt=""
-                  />
-                  <img
-                    src="https://www.pandasecurity.com/mediacenter/src/uploads/2013/11/pandasecurity-facebook-photo-privacy.jpg"
-                    alt=""
-                  />
-                </div>
-              </div>
-              <div className="timeline__BottomFriends">
-                <h3>Friends</h3>
-                <p>17 mutual Friends</p>
-                <div className="timeline__BottomFriendsContainer">
-                  <FriendCard />
-                  <FriendCard />
-                  <FriendCard />
-                  <FriendCard />
-                  <FriendCard />
-                  <FriendCard />
-                  <FriendCard />
-                  <FriendCard />
-                </div>
+                <img
+                  src="https://www.pandasecurity.com/mediacenter/src/uploads/2013/11/pandasecurity-facebook-photo-privacy.jpg"
+                  alt=""
+                />
+                <img
+                  src="https://www.pandasecurity.com/mediacenter/src/uploads/2013/11/pandasecurity-facebook-photo-privacy.jpg"
+                  alt=""
+                />
+                <img
+                  src="https://www.pandasecurity.com/mediacenter/src/uploads/2013/11/pandasecurity-facebook-photo-privacy.jpg"
+                  alt=""
+                />
               </div>
             </div>
-            <div className="timeline__BottomRight">
-              <Post />
+            <div className="timeline__bottomFriends">
+              <h3>Friends</h3>
+              <p>17 mutual Friends</p>
+              <div className="timeline__bottomFriendsContainer">
+                <FriendCard />
+                <FriendCard />
+                <FriendCard />
+                <FriendCard />
+                <FriendCard />
+                <FriendCard />
+                <FriendCard />
+                <FriendCard />
+              </div>
             </div>
+          </div>
+          <div className="timeline__bottomRight">
+            <Post />
           </div>
         </div>
       </div>
