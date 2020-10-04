7de5e64b4b8963da1ee359df5bce6c551f5bdc9d
korathbasil <korathbasil@live.com>
2020-10-05 00:07:46 +0530

    added routing for tab navigation in profile page

--- src/components/MainBody/Profile/Profile.jsx
+++ src/components/MainBody/Profile/Profile.jsx
@@ -104,9 +104,7 @@ function Profile() {
         <Route path="/myProfile">
           <Timeline photos={photos} />
         </Route>
-        <Route path="/myProfile/friends">
-          <ProfileFriends />
-        </Route>
+        <Route path="/myProfile/friends" component={ProfileFriends} />
       </Switch>
     </div>
   );
