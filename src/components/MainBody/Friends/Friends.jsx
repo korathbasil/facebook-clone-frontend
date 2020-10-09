8217408c17d3bdce4283d9168f13a7c692e41471
korathbasil <korathbasil@live.com>
2020-10-10 00:41:42 +0530

    added header to friends

--- src/components/MainBody/Friends/Friends.jsx
+++ src/components/MainBody/Friends/Friends.jsx
@@ -1,39 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Friends.css";
 import "./FriendNameCard/FriendNameCard";
+import Header from "../../Header/Header";
 import FriendNameCard from "./FriendNameCard/FriendNameCard";
 import Profile from "../Profile/Profile";
 import BG from "./bg.svg";
 
 function Friends() {
+  const [selectedFriend, setSelectedFriend] = useState(false);
   return (
     <div className="friends">
-      <div className="friends__sidebar">
-        <div className="friends__sidebarHeader">
-          <h2>Friends</h2>
+      <Header />
+      <div className="friends__container">
+        <div className="friends__sidebar">
+          <div className="friends__sidebarHeader">
+            <h2>Friends</h2>
+          </div>
+          <div className="friends__sidebarReqInfoContainer">
+            <h4>9 requests</h4>
+            <p>View sent requests</p>
+          </div>
+          <div className="friends__sidebarReqContainer">
+            <FriendNameCard onClick={() => setSelectedFriend(true)} />
+            <FriendNameCard />
+            <FriendNameCard />
+          </div>
+          <div className="friends__sidebarPeopleLists">
+            <h3>People you may know</h3>
+            <FriendNameCard />
+            <FriendNameCard />
+            <FriendNameCard />
+          </div>
         </div>
-        <div className="friends__sidebarReqInfoContainer">
-          <h4>9 requests</h4>
-          <p>View sent requests</p>
+        <div className="friends__main">
+          {selectedFriend && <Profile />}
+          {!selectedFriend && (
+            <div className="friends__mainDummy">
+              <img src={BG} alt="" />
+              <h3>Select people names to preview thir profile</h3>
+            </div>
+          )}
         </div>
-        <div className="friends__sidebarReqContainer">
-          <FriendNameCard />
-          <FriendNameCard />
-          <FriendNameCard />
-        </div>
-        <div className="friends__sidebarPeopleLists">
-          <h3>People you may know</h3>
-          <FriendNameCard />
-          <FriendNameCard />
-          <FriendNameCard />
-        </div>
-      </div>
-      <div className="friends__main">
-        <Profile />
-        {/* <div className="friends__mainDummy">
-          <img src={BG} alt="" />
-          <h3>Select people names to preview thir profile</h3>
-        </div> */}
       </div>
     </div>
   );
