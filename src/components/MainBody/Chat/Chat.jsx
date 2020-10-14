2cc211042ee82cb2e507e0ec78c6b080bb2e1bb4
korathbasil <korathbasil@live.com>
2020-10-14 23:51:16 +0530

    added message compont to chat

--- src/components/MainBody/Chat/Chat.jsx
+++ src/components/MainBody/Chat/Chat.jsx
@@ -4,6 +4,8 @@ import "./Chat.css";
 import Avatar from "@material-ui/core/Avatar";
 import CloseIcon from "@material-ui/icons/Close";
 import RemoveIcon from "@material-ui/icons/Remove";
+import ImageIcon from "@material-ui/icons/Image";
+import Message from "./Message/Message";
 
 function Chat() {
   return (
@@ -22,8 +24,11 @@ function Chat() {
           </div>
         </div>
       </div>
-      <div className="chat__body"></div>
+      <div className="chat__body">
+        <Message />
+      </div>
       <div className="chat__foot">
+        <ImageIcon />
         <form>
           <input type="text" />
           <button type="submit">Send</button>
--- src/components/MainBody/Chat/ChatBubble/ChatBubble.jsx
+++ src/components/MainBody/Chat/ChatBubble/ChatBubble.jsx
@@ -6,6 +6,10 @@ function ChatBubble() {
   return (
     <div className="chatBubble">
       <Avatar />
+      {/* <div className="chatBubble__info">
+        <h4>sagar SAG</h4>
+        <p>You : How are you mahn</p>
+      </div> */}
     </div>
   );
 }
