825c3fefc55f057aa5baa629a30c46f4f73ec0f7
korathbasil <korathbasil@live.com>
2020-10-23 04:29:18 +0530

    added user searching with support for partial search

--- src/components/Header/SearchModal/SearchModal.jsx
+++ src/components/Header/SearchModal/SearchModal.jsx
@@ -1,9 +1,36 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./SearchModal.css";
 import ArrowBackIcon from "@material-ui/icons/ArrowBack";
 import SearchIcon from "@material-ui/icons/Search";
+import axios from "../../../axios";
+import Axios from "axios";
+import useStateContext from "../../../context/DataLayer";
+import { Avatar } from "@material-ui/core";
 
 function SearchModal({ setShowSearchModalStatus }) {
+  const [{ token }, dispatch] = useStateContext();
+  const [searchText, setSearchText] = useState("");
+  const [searchedUsers, setSearchedUsers] = useState([]);
+  useEffect(() => {
+    const source = Axios.CancelToken.source();
+    if (searchText != "") {
+      axios
+        .post("/user/search", {
+          headers: {
+            "auth-token": token,
+          },
+          searchText: searchText,
+        })
+        .then((result) => {
+          setSearchedUsers(result.data);
+        })
+        .catch((e) => console.log(e));
+    }
+
+    return () => {
+      source.cancel();
+    };
+  }, [searchText]);
   return (
     <div className="searchModal">
       <div className="searchModal__header">
@@ -13,10 +40,28 @@ function SearchModal({ setShowSearchModalStatus }) {
         />
         <div className="searchModal__headerSearch">
           <SearchIcon className="searchModal__headerSearchicon" />
-          <input type="text" placeholder="Search Facebook" />
+          <input
+            type="text"
+            placeholder="Search Facebook"
+            value={searchText}
+            onChange={(e) => {
+              setSearchText(e.target.value);
+              if (e.target.value === "") {
+                setSearchedUsers([]);
+              }
+            }}
+          />
         </div>
       </div>
       <p>Recent Searches</p>
+      {searchedUsers?.map((user) => {
+        return (
+          <div className="searchModal__result">
+            <Avatar />
+            <p>{user?.displayName}</p>
+          </div>
+        );
+      })}
     </div>
   );
 }
