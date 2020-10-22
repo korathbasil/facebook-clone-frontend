fcc2619d969c729b3fdd01ec4dce7f20fa4c9c88
korathbasil <korathbasil@live.com>
2020-10-22 23:40:13 +0530

    story card edited with better styling

--- src/components/MainBody/Home/Feed/StoryCard/StoryCard.jsx
+++ src/components/MainBody/Home/Feed/StoryCard/StoryCard.jsx
@@ -7,7 +7,7 @@ function StoryCard() {
   return (
     <div className="storyCard">
       <div className="storyCard__avatar">
-        <Avatar />
+        <Avatar src="https://images.unsplash.com/photo-1506794778202-cad84cf45f1d?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjEyMDd9" />
       </div>
       <img
         src="https://images.pexels.com/photos/736230/pexels-photo-736230.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
