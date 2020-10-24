b7ac491a198b5f76569c199e3d805900929f2ef0
korathbasil <korathbasil@live.com>
2020-10-25 04:46:45 +0530

    add modal edited and added items

--- src/components/Header/AddModal/AddModal.jsx
+++ src/components/Header/AddModal/AddModal.jsx
@@ -1,10 +1,31 @@
 import React from "react";
 import "./AddModal.css";
+// Material UI elements\
+import CreateIcon from "@material-ui/icons/Create";
+import ImportContactsIcon from "@material-ui/icons/ImportContacts";
 
 function AddModal() {
   return (
     <div className="addModal">
-      <h2>Add</h2>
+      <h2>Create</h2>
+      <div className="addModal__item">
+        <div className="addModal__itemIcon">
+          <CreateIcon />
+        </div>
+        <div className="addModal__itemName">
+          <p>Post</p>
+          <p>Share a post on news feed</p>
+        </div>
+      </div>
+      <div className="addModal__item">
+        <div className="addModal__itemIcon">
+          <ImportContactsIcon />
+        </div>
+        <div className="addModal__itemName">
+          <p>Story</p>
+          <p>Share a photo or whrite something</p>
+        </div>
+      </div>
     </div>
   );
 }
